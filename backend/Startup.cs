957cead2b414888606bd5c7f9b51bb86f3165973
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-01-17 19:03:45 -0800

    Startup.cs is wired up to serve Vue frontend using spa static files in prod and localhost in dev

--- backend/Startup.cs
+++ backend/Startup.cs
@@ -32,8 +32,9 @@ namespace backend
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
-            services.AddControllersWithViews();
-            services.AddRazorPages();
+            // services.AddControllersWithViews();
+            // services.AddRazorPages();
+            services.AddSpaStaticFiles(options => { options.RootPath = "wwwroot"; });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,21 +51,32 @@ namespace backend
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+            // app.UseHttpsRedirection();
+            // app.UseStaticFiles();
 
             app.UseRouting();
 
             app.UseAuthentication();
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+
+            app.UseSpaStaticFiles();
+            app.UseSpa(builder =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-                endpoints.MapRazorPages();
+                if (env.IsDevelopment())
+                {
+                    builder.UseProxyToSpaDevelopmentServer(Configuration["ApplicationSettings:Client_URL"].ToString());
+                }
             });
+
+            // app.UseEndpoints(endpoints =>
+            // {
+            //     endpoints.MapControllerRoute(
+            //        name: "default",
+            //        pattern: "{controller=Home}/{action=Index}/{id?}");
+            //    endpoints.MapRazorPages();
+            // });
         }
     }
 }
