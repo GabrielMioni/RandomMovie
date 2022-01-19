2bffbb186c654e9fe330198b472fb09c7b9737c3
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-01-18 20:07:19 -0800

    Set configuration for passwords in Startup.ConfigureServices

--- backend/Startup.cs
+++ backend/Startup.cs
@@ -35,6 +35,15 @@ namespace backend
             // services.AddControllersWithViews();
             // services.AddRazorPages();
             services.AddSpaStaticFiles(options => { options.RootPath = "wwwroot"; });
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 4;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
