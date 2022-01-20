86f638697c37cbb580afefe1a00dd9881b3a6821
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-01-19 20:36:04 -0800

    Added UserController, which includes a Register method

---
+++ backend/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using backend.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System;
+using System.Threading.Tasks;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        private UserManager<IdentityUser> _userManager;
+        private SignInManager<IdentityUser> _signInManager;
+        private readonly ApplicationSettings _appSettings;
+
+        public UserController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, IOptions<ApplicationSettings> appSettings)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _appSettings = appSettings.Value;
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        // POST : /api/
+        public async Task<Object> Register(IdentityUserModel model)
+        {
+            var applicationUser = new IdentityUser()
+            {
+                UserName = model.UserName,
+                Email = model.Email
+            };
+
+            try
+            {
+                var result = await _userManager.CreateAsync(applicationUser, model.Password);
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+        }
+    }
+}
