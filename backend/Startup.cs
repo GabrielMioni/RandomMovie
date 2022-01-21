2752af8b6a9eb05fdacc76dc3bec8eab6beb074f
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-01-20 20:01:31 -0800

    Startup.Configuration calls AddRoles on AddDefaultIdentity

--- backend/Startup.cs
+++ backend/Startup.cs
@@ -47,6 +47,7 @@ namespace backend
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             // services.AddControllersWithViews();
             // services.AddRazorPages();
