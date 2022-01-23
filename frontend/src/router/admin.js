c8a24e4b607a774368bda75b077068ad6a2812eb
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-01-23 15:16:34 -0800

    admin route beforeEnter checks expires value and compares it to current time

--- frontend/src/router/admin.js
+++ frontend/src/router/admin.js
@@ -6,7 +6,13 @@ export const adminRoutes = [
     name: 'admin',
     component: () => import(/* webpackChunkName: "login" */ '../views/admin/AdminHome.vue'),
     beforeEnter (to, from, next) {
-      if (store.getters['user/role'] !== 'Admin') {
+      const expires = store.getters['user/expires']
+
+      const now = new Date().getTime()
+      const expiresDate = new Date(expires).getTime()
+      const isExpired = now > expiresDate
+
+      if (store.getters['user/role'] !== 'Admin' || isExpired) {
         next({ name: 'login' })
         return
       }
