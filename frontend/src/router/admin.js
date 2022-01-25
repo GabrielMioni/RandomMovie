836fd63390ea3a564cb775e59f935b8e2de20814
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-01-24 19:38:11 -0800

    beforeEach in admin routes retrieves role/expires data from localStorage instead of vuex state

--- frontend/src/router/admin.js
+++ frontend/src/router/admin.js
@@ -1,18 +1,16 @@
-import store from '@/store'
-
 export const adminRoutes = [
   {
     path: '/admin',
     name: 'admin',
     component: () => import(/* webpackChunkName: "login" */ '../views/admin/AdminHome.vue'),
     beforeEnter (to, from, next) {
-      const expires = store.getters['user/expires']
+      const { expires, role } = JSON.parse(localStorage.getItem('user'))
 
       const now = new Date().getTime()
       const expiresDate = new Date(expires).getTime()
       const isExpired = now > expiresDate
 
-      if (store.getters['user/role'] !== 'Admin' || isExpired) {
+      if (role !== 'Admin' || isExpired) {
         next({ name: 'login' })
         return
       }
