7632a6d274a2a1b2c5d5c8ca1ee380c93fa21403
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-01-25 19:49:33 -0800

    Added TestController to play with the HtmlReaderService

---
+++ backend/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AngleSharp;
+using AngleSharp.Html.Parser;
+using AngleSharp.Dom;
+using backend.services;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private HtmlReader _htmlReader;
+
+        public TestController(HtmlReader htmlReader)
+        {
+            _htmlReader = htmlReader;
+        }
+
+        [HttpGet]
+        [Route("Values")]
+        // GET : /api/Test
+        public IActionResult GetTest()
+        {
+            // return Ok(new { Hello = "just wanted to say hi" });
+            var result = _htmlReader.ParseHtml("https://www.criterionchannel.com/");
+            return Ok(result);
+        }
+    }
+}
