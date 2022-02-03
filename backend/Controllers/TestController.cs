5cd7ee0c9af0fc5f94624603ffadb2edf5158592
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-02-02 19:13:13 -0800

    TestController.GetFiltersAsync retrieves movie filters data

--- backend/Controllers/TestController.cs
+++ backend/Controllers/TestController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using AngleSharp;
-using AngleSharp.Html.Parser;
 using AngleSharp.Dom;
-using backend.services;
+using backend.Services;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using backend.Dtos;
 
 namespace backend.Controllers
 {
@@ -18,13 +20,124 @@ namespace backend.Controllers
         }
 
         [HttpGet]
+        [Route("Filters")]
+        public async Task<IActionResult> GetFiltersAsync()
+        {
+            var htmlCollection = await _htmlReader.ParseHtmlAsync("https://films.criterionchannel.com/", "main");
+
+            var main = htmlCollection.Length > 0 ? htmlCollection[0] : null;
+
+            var genres = GetFilterData(main, "genre");
+            var decades = GetFilterData(main, "decade");
+            var countries = GetFilterData(main, "country");
+            var directors = getDirectors(main);
+
+            return Ok(new { genres, decades, countries, directors });
+        }
+
+        private List<string> GetFilterData(IElement main, string labelName)
+        {
+            var labelSelector = string.Format("*[data-filter-group='{0}'] label", labelName);
+            var labelElms = main.QuerySelectorAll(labelSelector);
+
+            var labelValues = new List<string>();
+
+            foreach(var label in labelElms)
+            {
+                labelValues.Add(label.InnerHtml.Trim());
+            }
+
+            return labelValues;
+        }
+
+        private List<DirectorDto> getDirectors(IElement main)
+        {
+            var tableRows = main.QuerySelectorAll("#gridview tbody tr");
+
+            var directorList = new List<DirectorDto>();
+
+            foreach(var tr in tableRows)
+            {
+                var directorStrings = GetInnerHtml(tr, ".criterion-channel__td--director")
+                    .Replace(" and ", ", ")
+                    .Replace(", and", ", ")
+                    .Replace("  ", " ")
+                    .Split(", ")
+                    .Select(directorName => directorName.Trim())
+                    .Distinct();
+                
+                foreach(var directorName in directorStrings)
+                {
+                    var alreadyInList = directorList.Find(directorInList => directorInList.FullName == directorName);
+
+                    if (alreadyInList != null)
+                    {
+                        continue;
+                    }
+
+                    string firstName = directorName;
+                    string lastName = directorName;
+                    string fullName = directorName;
+
+                    var indexOfVon = directorName.ToLower().IndexOf(" von ");
+                    var index = indexOfVon > -1
+                        ? indexOfVon
+                        : directorName.LastIndexOf(' ');
+
+                    if (index > -1)
+                    {
+                        firstName = directorName.Substring(0, index);
+                        lastName = directorName.Substring(index + 1);
+                    }
+
+                    var directorDto = new DirectorDto
+                    {
+                        FirstName = firstName,
+                        LastName = lastName,
+                        FullName = fullName
+                    };
+
+                    directorList.Add(directorDto);
+                }
+            }
+
+            return directorList.OrderBy(d => d.LastName).ToList();
+        }
+
+        [HttpGet]
         [Route("Values")]
         // GET : /api/Test
-        public IActionResult GetTest()
+        public async Task<IActionResult> GetTestAsync()
         {
-            // return Ok(new { Hello = "just wanted to say hi" });
-            var result = _htmlReader.ParseHtml("https://www.criterionchannel.com/");
-            return Ok(result);
+            // var result = await _htmlReader.ParseHtmlAsync("https://films.criterionchannel.com//", "#gridview tbody tr .criterion-channel__td--title a");
+            var tableRows = await _htmlReader.ParseHtmlAsync("https://films.criterionchannel.com/", "#gridview tbody tr");
+
+            var movieList = new List<object>();
+            var directorList = new List<string>();
+
+            foreach(var tr in tableRows)
+            {
+                // var movieTitle = tr.QuerySelector(".criterion-channel__td--title a").InnerHtml.Trim();
+                var title = GetInnerHtml(tr, ".criterion-channel__td--title a");
+                var director = GetInnerHtml(tr, ".criterion-channel__td--director");
+                var country = GetInnerHtml(tr, ".criterion-channel__td--country span");
+                var year = GetInnerHtml(tr, ".criterion-channel__td--year");
+
+                // movieList.Add(new { title, director, country });
+                movieList.Add(new { country, director, title, year });
+                directorList.Add(director);
+            }
+
+            var count = movieList.Count;
+            var data = movieList;
+            var directors = directorList.Distinct().ToList();
+
+            return Ok(new { count, data, directors });
+        }
+
+        private string GetInnerHtml(IElement elm, string querySelector)
+        {
+            return elm.QuerySelector(querySelector).InnerHtml.Trim();
         }
     }
 }
