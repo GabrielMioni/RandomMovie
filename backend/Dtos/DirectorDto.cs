8ec8ffc0cf698dc59d5bc37086c517990241ac70
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-02-03 20:30:48 -0800

    Added DirectorDtos

---
+++ backend/Dtos/DirectorDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Dtos
+{
+    public class DirectorDto
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
