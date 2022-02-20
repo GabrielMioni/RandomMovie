184cb47995f5c9ab06d7457ba72e6d6c72f00244
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-02-19 20:48:39 -0800

    Added FilterService.cs

---
+++ backend/services/FilterService.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace backend.Services
+{
+    public class FilterService
+    {
+        public List<string> GetDirectorNamesFromString(string directorString)
+        {
+            directorString = Regex.Replace(directorString, "( and )|( , and)", ", ").Replace(",,", ",");
+
+            return directorString
+                .Split(", ")
+                .Select(directorName => Regex.Replace(directorName.Trim(), @"\s+", " ")) // Convert any whitespace to a single space
+                .Distinct()
+                .ToList();
+        }
+    }
+}
