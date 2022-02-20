71680b6591c4cd35a2c76a944c1eff7b489cb9e3
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-02-19 20:48:51 -0800

    Added GenreDto and MovieDto

---
+++ backend/Dtos/MovieDto.cs
@@ -0,0 +1,15 @@
+﻿using backend.Models.Filters;
+using System.Collections.Generic;
+
+namespace backend.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+        public Country Country { get; set; }
+        public Decade Decade { get; set; }
+        public string Title { get; set; }
+        public List<Director> Directors { get; set; }
+        public List<Genre> Genres { get; set; }
+    }
+}
