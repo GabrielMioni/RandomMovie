e58046fd64db78c56cf4ef44cf07f0afefec03ee
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-02-22 18:28:03 -0800

    Added MovieProfile.cs which maps Movie -> MovieDto

---
+++ backend/MappingProfiles/MovieProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using backend.Dtos;
+using backend.Models;
+using System.Linq;
+
+namespace backend.MappingProfiles
+{
+    public class MovieProfile : Profile
+    {
+        public MovieProfile()
+        {
+            CreateMap<Movie, MovieDto>()
+                .ForMember(dest => dest.Directors, opt => opt.MapFrom(src => src.Movie_Directors.Select(md => md.Director)))
+                .ForMember(dest => dest.Genres, opt => opt.MapFrom(src => src.Movie_Genres.Select(mg => mg.Genre)));
+            CreateMap<MovieDto, Movie>();
+        }
+    }
+}
