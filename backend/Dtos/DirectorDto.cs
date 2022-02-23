65d20251d878524c3af8e27660756504b40db534
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-02-22 19:37:00 -0800

    DirectorDto has Name (not FullName) which maps correctly from Director model.

--- backend/Dtos/DirectorDto.cs
+++ backend/Dtos/DirectorDto.cs
@@ -8,7 +8,7 @@ namespace backend.Dtos
     public class DirectorDto
     {
         public int Id { get; set; }
-        public string FullName { get; set; }
+        public string Name { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
     }
