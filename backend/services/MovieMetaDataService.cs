1e33eaaee89b519869d5a6eb0bd113a5285e52ea
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-02-27 14:54:00 -0800

    Added MovieMetaDataService.GetMetaDataForMovie()

--- backend/services/MovieMetaDataService.cs
+++ backend/services/MovieMetaDataService.cs
@@ -1,10 +1,17 @@
-﻿using backend.Models.Deserializers;
+﻿using AutoMapper;
+using backend.Data;
+using backend.Dtos;
+using backend.Models;
+using backend.Models.Deserializers;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Text.Json;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace backend.Services
@@ -12,14 +19,102 @@ namespace backend.Services
     public class MovieMetaDataService
     {
         private readonly IConfiguration _config;
+        private ApplicationDbContext _context;
+        private IMapper _mapper;
         private readonly string ApiKey;
         private readonly string RootUrl;
+        private readonly LevenshteinDistanceService _levService;
 
-        public MovieMetaDataService(IConfiguration config)
+        public MovieMetaDataService(ApplicationDbContext context, IConfiguration config, LevenshteinDistanceService levenshteinDistanceService, IMapper mapper)
         {
+            _context = context;
             _config = config;
+            _mapper = mapper;
             ApiKey = _config["MovieDbApi:ApiKey"];
             RootUrl = _config["MovieDbApi:Url"];
+            _levService = levenshteinDistanceService;
+        }
+
+        public void GetMetaDataForMovie()
+        {
+            // var movie _context.Movies.
+            var movieFromDb = _context.Movies
+                .Include(m => m.Country)
+                .Include(m => m.Decade)
+                .Include(m => m.Movie_Directors)
+                .ThenInclude(md => md.Director)
+                .Include(m => m.Movie_Genres)
+                .ThenInclude(mg => mg.Genre)
+                .FirstOrDefault(m => m.Title == "Mulholland Dr.");
+
+            if (movieFromDb == null) return;
+
+            var movieDto = _mapper.Map<MovieDto>(movieFromDb);
+
+            var movieSearchByTitle = SearchMovieByTitle(movieDto.Title);
+
+            var results = movieSearchByTitle.results;
+
+            foreach (var movieResult in results)
+            {
+                var releasedYearInt = Int32.Parse(movieResult.release_date.Split('-')[0]);
+
+                if (releasedYearInt < 1900)
+                {
+                    continue;
+                }
+
+                var yearDifference = GetYearDifference(releasedYearInt, movieDto.Year);
+
+                if (yearDifference > 2)
+                {
+                    continue;
+                }
+
+                var distance = LevenshteinDistanceService.Compute(movieResult.title, movieDto.Title);
+
+                if (distance > 10)
+                {
+                    continue;
+                }
+
+                if (distance > 2)
+                {
+                    var directorMatched = CheckMovieDirectorByApiId(movieResult.id, movieDto.Directors);
+                    if (!directorMatched)
+                    {
+                        continue;
+                    }
+                }
+
+                // add movie meta data
+            }
+        }
+
+        private int GetYearDifference (int yearA, int yearB)
+        {
+            return yearA > yearB? yearA - yearB: yearB - yearA;
+        }
+
+        private bool CheckMovieDirectorByApiId(int apiId, List<DirectorDto> directors)
+        {
+            var credits = GetMovieCreditsByApiId(apiId);
+            var directorNamesFromApi = credits.crew
+                .FindAll(c => c.job == "Director")
+                .Select(c => c.name)
+                .ToList();
+
+            var directorNamesFromDb = directors.Select(d => d.Name);
+
+            foreach (var name in directorNamesFromApi)
+            {
+                if (directorNamesFromDb.Contains(name))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
         public Credits GetMovieCreditsByApiId(int movieId)
