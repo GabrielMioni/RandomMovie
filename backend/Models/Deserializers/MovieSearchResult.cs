3cb0518e1b21d431346bc956cc86a6f4e7ecbd9c
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-02-27 15:00:47 -0800

    Added MovieSearchResult.releae_date

--- backend/Models/Deserializers/MovieSearchResult.cs
+++ backend/Models/Deserializers/MovieSearchResult.cs
@@ -7,6 +7,7 @@
         public string original_title { get; set; }
         public string overview { get; set; }
         public string poster_path { get; set; }
+        public string release_date { get; set; }
         public string title { get; set; }
     }
 }
