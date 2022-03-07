c23cb5d57c3dc7e3ca13a69bb1472fd1148cdbb8
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-03-06 16:56:43 -0800

    Removed some whitespace on Movie_Director.cs

--- backend/Models/Movie_Director.cs
+++ backend/Models/Movie_Director.cs
@@ -8,13 +8,9 @@ namespace backend.Models.Filters
     public class Movie_Director
     {
         public int Id { get; set; }
-
         public int MovieId { get; set; }
-
         public Movie Movie { get; set; }
-
         public int DirectorId { get; set; }
-
         public Director Director { get; set; }
     }
 }
