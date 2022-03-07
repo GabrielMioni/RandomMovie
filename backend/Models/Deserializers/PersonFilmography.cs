ca57357415d4b5b61bec01f195d52fb814b5907b
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-03-06 17:02:06 -0800

    Added PersonFilmography deserializer

---
+++ backend/Models/Deserializers/PersonFilmography.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace backend.Models.Deserializers
+{
+    public class PersonFilmography
+    {
+        public int id { get; set; }
+        public List<MovieSearchResult> cast { get; set; }
+        public List<MovieSearchResult> crew { get; set; }
+    }
+}
