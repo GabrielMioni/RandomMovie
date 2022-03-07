c6350e88823e77a40e7ef53664f98b5bf251e94d
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-03-06 16:55:26 -0800

    Added Movies property on DirectorDto

--- backend/Dtos/DirectorDto.cs
+++ backend/Dtos/DirectorDto.cs
@@ -11,5 +11,6 @@ namespace backend.Dtos
         public string Name { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public List<MovieDto> Movies { get; set; }
     }
 }
