24b3194989aa9514a5cba211f095fdbd3284de11
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-03-06 16:59:15 -0800

    Added MovieMeta model

---
+++ backend/Models/MovieMeta.cs
@@ -0,0 +1,14 @@
+﻿namespace backend.Models
+{
+    public class MovieMeta
+    {
+        public int Id { get; set; }
+        public string OriginalTitle { get; set; }
+        public string OriginalLanguage { get; set; }
+        public string Overview { get; set; }
+        public string PosterPath { get; set; }
+        public string ReleasedDate { get; set; }
+        public string Title { get; set; }
+        public int MovieId { get; set; }
+    }
+}
