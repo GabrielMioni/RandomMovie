c5a11a0e35a54774ffb3f69e620882a19cd9d3c4
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-03-06 17:01:17 -0800

    Added PersonSearch deserializer

---
+++ backend/Models/Deserializers/PersonSearch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace backend.Models.Deserializers
+{
+    // TODO: Extract PersonSearchResult to its own file
+    public class PersonSearchResult
+    {
+        public int id { get; set; }
+        public List<MovieSearchResult> known_for { get; set; }
+        public string known_for_department { get; set; }
+        public string name { get; set; }
+    }
+
+    public class PersonSearch
+    {
+        public int page { get; set; }
+        public int total_pages { get; set; }
+        public int total_results { get; set; }
+        public List<PersonSearchResult> results { get; set; }
+    }
+}
