58b69cf904ad05596e8112fc9333742fc3a6762a
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-03-12 15:59:45 -0800

    Added mapping profile for MovieSearchResult -> MovieMeta

--- backend/MappingProfiles/MovieProfile.cs
+++ backend/MappingProfiles/MovieProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using backend.Dtos;
 using backend.Models;
+using backend.Models.Deserializers;
 using backend.Models.Filters;
 using System.Linq;
 
@@ -25,7 +26,15 @@ namespace backend.MappingProfiles
                 .ForMember(dest => dest.Year, opt => opt.MapFrom(src => src.Movie.Year))
                 .ForMember(dest => dest.Genres, opt => opt.MapFrom(src => src.Movie.Movie_Genres.Select(mg => mg.Genre)));
 
-
+            CreateMap<MovieSearchResult, MovieMeta>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.ApiId, opt => opt.MapFrom(src => src.id))
+                .ForMember(dest => dest.OriginalLanguage, opt => opt.MapFrom(src => src.original_language))
+                .ForMember(dest => dest.OriginalTitle, opt => opt.MapFrom(src => src.original_title))
+                .ForMember(dest => dest.Overview, opt => opt.MapFrom(src => src.overview))
+                .ForMember(dest => dest.PosterPath, opt => opt.MapFrom(src => src.poster_path))
+                .ForMember(dest => dest.ReleasedDate, opt => opt.MapFrom(src => src.release_date))
+                .ForMember(dest => dest.Title, opt => opt.MapFrom(src => src.title));
         }
     }
 }
