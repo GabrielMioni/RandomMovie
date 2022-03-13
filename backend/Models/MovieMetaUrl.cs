79f276f1f1eaf696d9a7c0ece10ecdc0bd4adc0f
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-03-13 16:32:44 -0700

    Added MetaImageSize and MovieMetaUrl models

---
+++ backend/Models/MovieMetaUrl.cs
@@ -0,0 +1,9 @@
+﻿namespace backend.Models
+{
+    public class MovieMetaUrl
+    {
+        public int Id { get; set; }
+        public string BaseUrl { get; set; }
+        public string SecureBaseUrl { get; set; }
+    }
+}
