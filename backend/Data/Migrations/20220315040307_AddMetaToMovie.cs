efa4812f5d9487029955e6d830d6f443a5621a8b
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-03-14 21:58:13 -0700

    Added Movie.Meta

---
+++ backend/Data/Migrations/20220315040307_AddMetaToMovie.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace backend.Data.Migrations
+{
+    public partial class AddMetaToMovie : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MovieMeta",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OriginalTitle = table.Column<string>(nullable: true),
+                    OriginalLanguage = table.Column<string>(nullable: true),
+                    Overview = table.Column<string>(nullable: true),
+                    PosterPath = table.Column<string>(nullable: true),
+                    ReleasedDate = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(nullable: true),
+                    MovieId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MovieMeta", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MovieMeta_Movies_MovieId",
+                        column: x => x.MovieId,
+                        principalTable: "Movies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MovieMeta_MovieId",
+                table: "MovieMeta",
+                column: "MovieId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MovieMeta");
+        }
+    }
+}
