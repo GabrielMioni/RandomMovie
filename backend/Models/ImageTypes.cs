bb61f700dd18bb9274027d96e968dceeafe973f9
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-03-14 20:01:46 -0700

    Added ImageTypes class to avoid using magic strings for the 3 types of images from MovieDBApi

---
+++ backend/Models/ImageTypes.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+
+namespace backend.Models
+{
+    public static class ImageTypes
+    {
+        public static string Poster = "poster";
+        public static string Backdrop = "backdrop";
+        public static string Logo = "logo";
+    }
+}
