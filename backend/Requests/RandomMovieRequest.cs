4a76475a569966b2e7cb490a090da66a22a5b2de
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-03-15 19:29:05 -0700

    Added RandomMovieRequest

---
+++ backend/Requests/RandomMovieRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace backend.Requests
+{
+    public class RandomMovieRequest
+    {
+        public List<int> CountryIds { get; set; }
+        public List<int> DecadeIds { get; set; }
+        public List<int> DirectorIds { get; set; }
+        public List<int> GenreIds { get; set; }
+    }
+}
