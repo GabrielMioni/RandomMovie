67116f8f7eb427025d978becebf51c7f457d7a0d
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-03-17 20:22:54 -0700

    ConfigurationPayload properties that are for image sizes are Dictionary<string, string> types.

---
+++ backend/Requests/ConfigurationPayload.cs
@@ -0,0 +1,13 @@
+﻿using backend.Dtos;
+using System.Collections.Generic;
+
+namespace backend.Requests
+{
+    public class ConfigurationPayload
+    {
+        public Dictionary<string, string> PosterSizes { get; set; }
+        public Dictionary<string, string> BackDropSizes { get; set; }
+        public Dictionary<string, string> LogoSizes { get; set; }
+        public BaseUrlsDto BaseUrls { get; set; }
+    }
+}
