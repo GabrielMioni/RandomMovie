a12ac53de1286b7d1d1dbccb67e757cd2c9cc1ff
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-03-17 19:40:09 -0700

    Renamed LoginModel to LoginRequest

--- backend/Requests/LoginRequest.cs
+++ backend/Requests/LoginRequest.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace backend.Requests
 {
-    public class LoginModel
+    public class LoginRequest
     {
         public string UserName { get; set; }
         public string Password { get; set; }
