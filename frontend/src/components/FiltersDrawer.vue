bb16b668c7ccdaed2c7dd84f1ca0b6c9c9876a10
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-03-20 14:51:43 -0700

    FiltersDrawer uses v-theme-provider to use light theme for drawer

--- frontend/src/components/FiltersDrawer.vue
+++ frontend/src/components/FiltersDrawer.vue
@@ -1,46 +1,48 @@
 <template>
-  <v-navigation-drawer
-    v-model="filterIsOpenLocal"
-    width="50%"
-    absolute
-    temporary
-    right>
-    <v-card
-      height="100%"
-      width="100%">
-      <v-toolbar elevation="0">
-        <v-spacer></v-spacer>
-        <v-btn
-          depressed
-          fab
-          color="transparent"
-          @click="close">
-          <v-icon icon>mdi-close</v-icon>
-        </v-btn>
-      </v-toolbar>
-      <v-card-text class="fill-height">
-        <v-container class="fill-height align-content-start">
-          <v-row class="fill-height">
-            <v-col cols="4">
-              <v-list>
-                <v-list-item
-                  v-for="(filter, index) in filterTypes"
-                  :key="`filter-${index}`">
-                  <v-list-item-title>
-                    {{ filter }}
-                  </v-list-item-title>
-                </v-list-item>
-              </v-list>
-            </v-col>
-            <v-divider vertical></v-divider>
-            <v-col cols="5">
-              Jangles.
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-card-text>
-    </v-card>
-  </v-navigation-drawer>
+  <v-theme-provider light>
+    <v-navigation-drawer
+      v-model="filterIsOpenLocal"
+      width="50%"
+      absolute
+      temporary
+      right>
+      <v-card
+        height="100%"
+        width="100%">
+        <v-toolbar elevation="0">
+          <v-spacer></v-spacer>
+          <v-btn
+            depressed
+            fab
+            color="transparent"
+            @click="close">
+            <v-icon icon>mdi-close</v-icon>
+          </v-btn>
+        </v-toolbar>
+        <v-card-text class="fill-height">
+          <v-container class="fill-height align-content-start">
+            <v-row class="fill-height">
+              <v-col cols="4">
+                <v-list>
+                  <v-list-item
+                    v-for="(filter, index) in filterTypes"
+                    :key="`filter-${index}`">
+                    <v-list-item-title>
+                      {{ filter }}
+                    </v-list-item-title>
+                  </v-list-item>
+                </v-list>
+              </v-col>
+              <v-divider vertical></v-divider>
+              <v-col cols="5">
+                Jangles.
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
+      </v-card>
+    </v-navigation-drawer>
+  </v-theme-provider>
 </template>
 
 <script>
