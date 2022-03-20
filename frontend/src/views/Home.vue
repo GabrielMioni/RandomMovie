780d91211ed1db2d144c8c636c35e6c739c00116
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-03-19 19:02:50 -0700

    Home component imports/uses RandomMovie

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
-  <hello-world />
+  <div>
+    <random-movie></random-movie>
+  </div>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld'
+// import HelloWorld from '../components/HelloWorld'
+import RandomMovie from '../components/RandomMovie.vue'
 
 export default {
   name: 'Home',
 
   components: {
-    HelloWorld
+    RandomMovie
   }
 }
 </script>
