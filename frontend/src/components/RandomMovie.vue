d4f2f35371b41f3c9caa3e088c84d62231aa3607
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-03-19 20:24:02 -0700

    RandomMovie can DISPLAY MOVIES!

--- frontend/src/components/RandomMovie.vue
+++ frontend/src/components/RandomMovie.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container>
+  <v-container class="fill-height">
     <v-row>
       <v-col>
         <v-btn
@@ -8,15 +8,31 @@
         </v-btn>
       </v-col>
     </v-row>
-    <v-row v-if="movie.title.trim().length > 0">
-      <v-col>
-        <v-card>
+    <v-row
+      v-if="movie.title.trim().length > 0">
+      <v-col
+        :cols="12"
+        class="d-flex justify-center">
+        <v-card width="80%">
           <v-card-text>
-            <movie-image
-              :image-path="movie.meta['posterPath']"
-              xl>
-            </movie-image>
-            {{ movie.title }}
+            <v-container fluid>
+              <v-row>
+                <v-col>
+                  <movie-image
+                    :image-path="movie.meta['posterPath']"
+                    xl>
+                  </movie-image>
+                </v-col>
+                <v-col>
+                  <h1 class="pb-3">{{ movie.title }}</h1>
+                  <p>
+                    Directed by {{ directedBy }} • {{ movie.year }} • {{ movie.country.name }} <br>
+                    {{ originalLanguage }}
+                  </p>
+                  <p>{{ movie.meta.overview }}</p>
+                </v-col>
+              </v-row>
+            </v-container>
           </v-card-text>
         </v-card>
       </v-col>
@@ -44,6 +60,23 @@ export default {
         title: '',
         year: 0
       }
+    }
+  },
+  computed: {
+    directedBy () {
+      const directedByString = this.movie.directors.map(director => director.name).join(', ')
+      const lastIndexComma = directedByString.lastIndexOf(',')
+      if (lastIndexComma < 0) {
+        return directedByString
+      }
+      return `${directedByString.substring(0, lastIndexComma)} and ${directedByString.substring(lastIndexComma + 1)}`
+    },
+    originalLanguage () {
+      if (Object.keys(this.movie.meta).length <= 0) {
+        return ''
+      }
+      const language = new Intl.DisplayNames(['en'], { type: 'language' })
+      return language.of(this.movie.meta.originalLanguage)
     }
   },
   methods: {
