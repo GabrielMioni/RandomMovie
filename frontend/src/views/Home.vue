54968612395e8d881c57557188e1308fdc279ad6
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-03-20 14:15:36 -0700

    Home component opens/closes FiltersDrawer

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
   <div>
+    <v-btn @click="toggleFilters">
+      Filters
+    </v-btn>
     <random-movie></random-movie>
+    <filters-drawer
+      :filters-open="filtersOpen"
+      @drawerIsOpen="toggleFilters">
+    </filters-drawer>
   </div>
 </template>
 
 <script>
 // import HelloWorld from '../components/HelloWorld'
 import RandomMovie from '../components/RandomMovie.vue'
+import FiltersDrawer from '@/components/FiltersDrawer'
 
 export default {
   name: 'Home',
-
+  data () {
+    return {
+      filtersOpen: false
+    }
+  },
   components: {
-    RandomMovie
+    RandomMovie,
+    FiltersDrawer
+  },
+  methods: {
+    toggleFilters (value) {
+      console.log({ clicko: value })
+      this.filtersOpen = value
+    }
   }
 }
 </script>
