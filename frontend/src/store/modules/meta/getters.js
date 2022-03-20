68275e23d334c04960281acba734735397506192
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-03-19 18:55:24 -0700

    Added secureBaseUrl, baseUrl and posterSizes getters on meta Vuex module

--- frontend/src/store/modules/meta/getters.js
+++ frontend/src/store/modules/meta/getters.js
@@ -1,3 +1,11 @@
-export default {
+const secureBaseUrl = (state) => state.secureBaseUrl
 
+const baseUrl = (state) => state.baseUrl
+
+const posterSizes = (state) => state.posterSizes
+
+export default {
+  baseUrl,
+  secureBaseUrl,
+  posterSizes
 }
