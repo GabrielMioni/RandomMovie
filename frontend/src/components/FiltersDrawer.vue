f169c7ed53ace7e91aa014859ac0180d5aaf6ea0
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-03-23 21:14:32 -0700

    Removed v-card from FiltersDrawer. Not needed structurally

--- frontend/src/components/FiltersDrawer.vue
+++ frontend/src/components/FiltersDrawer.vue
@@ -3,159 +3,156 @@
     <v-navigation-drawer
       v-model="filterIsOpenLocal"
       width="50%"
+      height="100vh"
       absolute
       temporary
       right>
-      <v-card
-        width="100%">
-        <v-toolbar elevation="0">
+      <v-container class="fill-height align-content-start">
+        <v-row>
           <v-spacer></v-spacer>
-          <v-btn
-            depressed
-            fab
-            color="transparent"
-            @click="close">
-            <v-icon icon>mdi-close</v-icon>
-          </v-btn>
-        </v-toolbar>
-        <v-card-text class="fill-height">
-          <v-container class="fill-height align-content-start">
-            <v-row class="fill-height">
-              <v-col
-                class="filters-col-left d-flex flex-column"
-                cols="4">
-                <v-row>
-                  <v-list>
-                    <v-list-item
-                      v-for="(filter, index) in filterTypes"
-                      :key="`filter-${index}`">
-                      <v-list-item-title
-                        @click="scrollToFilter(filter)">
-                        <div class="filters-col-left__type">{{ filter }}</div>
-                      </v-list-item-title>
-                    </v-list-item>
-                  </v-list>
-                </v-row>
-                <v-row>
-                  <v-col class="d-flex flex-column justify-end align-content-end">
-                    <div class="align-content-center justify-center">
-                      <v-btn
-                        class="mr-3"
-                        outlined
-                        @click="resetAllFilters">
-                        Reset
-                      </v-btn>
-                      <v-btn
-                        outlined
-                        @click="applyFilters">
-                        Apply
-                      </v-btn>
-                    </div>
-                  </v-col>
-                </v-row>
-              </v-col>
-              <v-divider vertical></v-divider>
-              <v-col
-                cols="8"
-                class="pa-1 overflow-y-auto overflow-x-hidden filters-col-right">
-                <v-row>
-                  <v-col cols="12">
-                    <h3
-                      ref="genres">
-                      Genres
-                    </h3>
-                  </v-col>
-                  <v-col
-                    v-for="genre in genres"
-                    :key="`genre-filter-${genre.id}`"
-                    class="py-0"
-                    cols="6">
-                    <v-checkbox
-                      :ref="`genres-checkbox-${genre.id}`"
-                      dense
-                      hide-details
-                      :label="genre.name"
-                      @click="clickCheckbox('selectedGenres', genre.id)">>
-                    </v-checkbox>
-                  </v-col>
-                </v-row>
-                <v-row>
-                  <v-col cols="12">
-                    <h3
-                      ref="decades">
-                      Decades
-                    </h3>
-                  </v-col>
-                  <v-col
-                    v-for="decade in decades"
-                    :key="`decade-filter-${decade.id}`"
-                    class="py-0"
-                    cols="6">
-                    <v-checkbox
-                      :ref="`decades-checkbox-${decade.id}`"
-                      dense
-                      hide-details
-                      :label="decade.name"
-                      @click="clickCheckbox('selectedDecades', decade.id)">
-                    </v-checkbox>
-                  </v-col>
-                </v-row>
-                <v-row>
-                  <v-col cols="12">
-                    <h3
-                      ref="countries">
-                      Countries
-                    </h3>
-                  </v-col>
-                  <v-col
-                    v-for="country in countries"
-                    :key="`country-filter-${country.id}`"
-                    class="py-0"
-                    cols="6">
-                    <v-checkbox
-                      :ref="`countries-checkbox-${country.id}`"
-                      dense
-                      hide-details
-                      :label="country.name"
-                      @click="clickCheckbox('selectedCountries', country.id)">
-                    </v-checkbox>
-                  </v-col>
-                </v-row>
-                <v-row>
-                  <v-col cols="12">
-                    <h3
-                      ref="directors">
-                      Directors
-                    </h3>
-                    <a id="#directors"></a>
-                  </v-col>
-                  <template
-                    v-for="(sortedDirectors, key) in directorsSorted">
-                    <v-col
-                      :key="`director-${key}`"
-                      cols="12">
-                      <h3>{{ key.toUpperCase()}}</h3>
-                    </v-col>
-                    <v-col
-                      v-for="director in sortedDirectors"
-                      :key="`director-filter-${director.id}`"
-                      class="py-0"
-                      cols="6">
-                      <v-checkbox
-                        :ref="`directors-checkbox-${director.id}`"
-                        dense
-                        hide-details
-                        :label="displayDirectorName(director)"
-                        @click="clickCheckbox('selectedDirectors', director.id)">
-                      </v-checkbox>
-                    </v-col>
-                  </template>
-                </v-row>
+          <v-col>
+            <v-btn
+              depressed
+              fab
+              color="transparent"
+              @click="close">
+              <v-icon icon>mdi-close</v-icon>
+            </v-btn>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col
+            class="filters-col-left d-flex flex-column"
+            cols="4">
+            <v-row>
+              <v-list>
+                <v-list-item
+                  v-for="(filter, index) in filterTypes"
+                  :key="`filter-${index}`">
+                  <v-list-item-title
+                    @click="scrollToFilter(filter)">
+                    <div class="filters-col-left__type">{{ filter }}</div>
+                  </v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-row>
+            <v-row>
+              <v-col class="d-flex flex-column justify-end align-content-end">
+                <div class="align-content-center justify-center">
+                  <v-btn
+                    class="mr-3"
+                    outlined
+                    @click="resetAllFilters">
+                    Reset
+                  </v-btn>
+                  <v-btn
+                    outlined
+                    @click="applyFilters">
+                    Apply
+                  </v-btn>
+                </div>
               </v-col>
             </v-row>
-          </v-container>
-        </v-card-text>
-      </v-card>
+          </v-col>
+          <v-col
+            cols="8"
+            class="pa-1 overflow-y-auto overflow-x-hidden filters-col-right">
+            <v-row>
+              <v-col cols="12">
+                <h3
+                  ref="genres">
+                  Genres
+                </h3>
+              </v-col>
+              <v-col
+                v-for="genre in genres"
+                :key="`genre-filter-${genre.id}`"
+                class="py-0"
+                cols="6">
+                <v-checkbox
+                  :ref="`genres-checkbox-${genre.id}`"
+                  dense
+                  hide-details
+                  :label="genre.name"
+                  @click="clickCheckbox('selectedGenres', genre.id)">>
+                </v-checkbox>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12">
+                <h3
+                  ref="decades">
+                  Decades
+                </h3>
+              </v-col>
+              <v-col
+                v-for="decade in decades"
+                :key="`decade-filter-${decade.id}`"
+                class="py-0"
+                cols="6">
+                <v-checkbox
+                  :ref="`decades-checkbox-${decade.id}`"
+                  dense
+                  hide-details
+                  :label="decade.name"
+                  @click="clickCheckbox('selectedDecades', decade.id)">
+                </v-checkbox>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12">
+                <h3
+                  ref="countries">
+                  Countries
+                </h3>
+              </v-col>
+              <v-col
+                v-for="country in countries"
+                :key="`country-filter-${country.id}`"
+                class="py-0"
+                cols="6">
+                <v-checkbox
+                  :ref="`countries-checkbox-${country.id}`"
+                  dense
+                  hide-details
+                  :label="country.name"
+                  @click="clickCheckbox('selectedCountries', country.id)">
+                </v-checkbox>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12">
+                <h3
+                  ref="directors">
+                  Directors
+                </h3>
+                <a id="#directors"></a>
+              </v-col>
+              <template
+                v-for="(sortedDirectors, key) in directorsSorted">
+                <v-col
+                  :key="`director-${key}`"
+                  cols="12">
+                  <h3>{{ key.toUpperCase()}}</h3>
+                </v-col>
+                <v-col
+                  v-for="director in sortedDirectors"
+                  :key="`director-filter-${director.id}`"
+                  class="py-0"
+                  cols="6">
+                  <v-checkbox
+                    :ref="`directors-checkbox-${director.id}`"
+                    dense
+                    hide-details
+                    :label="displayDirectorName(director)"
+                    @click="clickCheckbox('selectedDirectors', director.id)">
+                  </v-checkbox>
+                </v-col>
+              </template>
+            </v-row>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-navigation-drawer>
   </v-theme-provider>
 </template>
@@ -300,7 +297,7 @@ export default {
 
 <style lang="scss" scoped>
 .filters-col-right {
-  height: 750px;
+  height: 90vh;
 }
 .filters-col-left {
   &__type {
