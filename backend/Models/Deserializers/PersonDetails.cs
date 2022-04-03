0452283bbcff2d22a89161d60b3aea61aea81e67
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-04-02 17:51:22 -0700

    Added PersonDetails deserializer

---
+++ backend/Models/Deserializers/PersonDetails.cs
@@ -0,0 +1,10 @@
+﻿namespace backend.Models.Deserializers
+{
+    public class PersonDetails
+    {
+        public string biography { get; set; }
+        public string birthday { get; set; }
+        public string deathday { get; set; }
+        public string place_of_birth { get; set; }
+    }
+}
