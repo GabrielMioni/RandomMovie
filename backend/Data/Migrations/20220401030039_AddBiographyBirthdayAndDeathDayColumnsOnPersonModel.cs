72f0557479cc881af7280a1e46bdf6706964128f
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-04-02 17:55:50 -0700

    Added Biography, Birthday and Deathday columns on Person model

---
+++ backend/Data/Migrations/20220401030039_AddBiographyBirthdayAndDeathDayColumnsOnPersonModel.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace backend.Data.Migrations
+{
+    public partial class AddBiographyBirthdayAndDeathDayColumnsOnPersonModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Biography",
+                table: "Persons",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Birthday",
+                table: "Persons",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Deathday",
+                table: "Persons",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Biography",
+                table: "Persons");
+
+            migrationBuilder.DropColumn(
+                name: "Birthday",
+                table: "Persons");
+
+            migrationBuilder.DropColumn(
+                name: "Deathday",
+                table: "Persons");
+        }
+    }
+}
