d3c4c4e538372a6f518e0ff1f6c96e0757279a9b
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-04-03 19:58:04 -0700

    Added Character column on Movie_Person

---
+++ backend/Data/Migrations/20220404011746_AddCharacterPropertyToMovie_Person.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace backend.Data.Migrations
+{
+    public partial class AddCharacterPropertyToMovie_Person : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Character",
+                table: "Movie_Persons",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Character",
+                table: "Movie_Persons");
+        }
+    }
+}
