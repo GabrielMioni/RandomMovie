e762d79cbc3e135e7460ddbeda878b2fb8299353
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-04-04 21:30:42 -0700

    Added getMovieById in /api/movies.js

--- frontend/src/api/movies.js
+++ frontend/src/api/movies.js
@@ -2,3 +2,6 @@ import axios from 'axios'
 
 export const getRandomMovie = (params = {}) =>
   axios.post('/api/Movies/GetRandomMovie', params)
+
+export const getMovieById = (movieId) =>
+  axios.get(`/api/Movies/${movieId}`)
