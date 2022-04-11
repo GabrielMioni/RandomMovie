f4c478e0148bae8aeb6ea4ba5f6b7a6ee510cfd3
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-04-10 21:25:07 -0700

    Added skeleton components for AdminMovies and AdminFilters

---
+++ frontend/src/views/admin/AdminFilters.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-card>
+          <v-card-text>
+            Welcome to Admin Filters
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'AdminFilters'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/views/admin/AdminMovies.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-card>
+          <v-card-text>
+            Welcome to Admin Movies
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'AdminMovies'
+}
+</script>
+
+<style scoped>
+
+</style>
