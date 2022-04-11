338a18104c59dbf0894c1a1f505c6b765e034dce
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-04-10 21:24:26 -0700

    Added AdminSidebar

---
+++ frontend/src/components/AdminSidebar.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-navigation-drawer
+    app
+    permanent>
+    <v-list-item>
+      <v-list-item-content>
+        <v-list-item-title class="text-h6">
+          <router-link to="/movie">
+            Random Movie
+          </router-link>
+        </v-list-item-title>
+        <v-list-item-subtitle>
+          Hello {{ userName }}
+        </v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+    <v-divider></v-divider>
+    <v-list-item
+      v-for="(route, index) in routes"
+      :key="`sidebar-route-${index}`">
+      <v-list-item-content>
+        <v-list-item-title>
+          <router-link :to="route.path">
+            {{ route.title }}
+          </router-link>
+        </v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: 'AdminSidebar',
+  data () {
+    return {
+      userName: '',
+      routes: [
+        {
+          path: '/admin/movies',
+          title: 'Movies'
+        },
+        {
+          path: '/admin/filters',
+          title: 'Filters'
+        }
+      ]
+    }
+  },
+  mounted () {
+    const userDataJson = localStorage.getItem('user')
+    const userData = JSON.parse(userDataJson)
+    this.userName = userData.userName
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
