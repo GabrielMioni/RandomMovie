72895c39fa21d3e62f831566c61f49b1aea60764
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-04-11 21:44:03 -0700

    Added GetMoviesPaginatedRequest and GetMOviesPaginatedResponse

---
+++ backend/Requests/GetMoviesPaginatedRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace backend.Requests
+{
+    public class GetMoviesPaginatedRequest
+    {
+        public int Page { get; set; }
+        public int Take { get; set; }
+    }
+}
---
+++ backend/Requests/GetMoviesPaginatedResponse.cs
@@ -0,0 +1,12 @@
+﻿using backend.Dtos;
+using System.Collections.Generic;
+
+namespace backend.Requests
+{
+    public class GetMoviesPaginatedResponse
+    {
+        public int Total { get; set; }
+        public int PageCount { get; set; }
+        public List<MovieDto> Movies { get; set; }
+    }
+}
