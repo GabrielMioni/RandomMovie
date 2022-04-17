cdcd92206c8499d9d1fd7f61de2ed29a1da0fe6a
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-04-17 15:19:51 -0700

    GetMoviesPaginatedRequest.SortBy and SortDesc are now List<string> and List<bool> to better match request data sent from FE and support multisort

--- backend/Requests/GetMoviesPaginatedRequest.cs
+++ backend/Requests/GetMoviesPaginatedRequest.cs
@@ -1,8 +1,12 @@
-﻿namespace backend.Requests
+﻿using System.Collections.Generic;
+
+namespace backend.Requests
 {
     public class GetMoviesPaginatedRequest
     {
         public int Page { get; set; }
         public int ItemsPerPage { get; set; }
+        public List<string> SortBy { get; set; }
+        public List<bool> SortDesc { get; set; }
     }
 }
