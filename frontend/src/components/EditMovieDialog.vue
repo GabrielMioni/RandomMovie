6ca2097c70b06504b7bc3837c5cde808c1bf4936
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-04-24 15:45:15 -0700

    Added EditMovieDialog

---
+++ frontend/src/components/EditMovieDialog.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-dialog
+    max-width="1200"
+    :fullscreen="$vuetify.breakpoint.smAndDown"
+    v-model="open">
+    <v-card>
+      <v-card-text>
+        <v-form>
+          <v-container>
+            <v-row>
+              <v-col>
+                <v-text-field
+                  v-model="movie.title"
+                  solo>
+                </v-text-field>
+              </v-col>
+              <v-col>
+                <v-text-field
+                  v-model="movie.title"
+                  solo>
+                </v-text-field>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: 'EditMovieDialog',
+  data () {
+    return {
+      movieLocal: {}
+    }
+  },
+  props: {
+    value: {
+      required: true,
+      type: Boolean
+    },
+    movie: {
+      required: true,
+      type: Object
+    }
+  },
+  watch: {
+    movie () {
+      this.movieLocal = { ...this.movie }
+    }
+  },
+  computed: {
+    open: {
+      get () {
+        return this.value
+      },
+      set (value) {
+        this.$emit('input', value)
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
