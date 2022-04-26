000db378dd95f63bdf38b63f0a836687efaa8399
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-04-25 19:09:54 -0700

    Added searchDirectors in /api/filters.js

--- frontend/src/api/filters.js
+++ frontend/src/api/filters.js
@@ -2,3 +2,8 @@ import axios from 'axios'
 
 export const getFilters = () =>
   axios.get('/api/Filters/Get')
+
+export const searchDirectors = (search = '') => {
+  const searchEncoded = search.trim().length > 0 ? encodeURI(search.trim()) : ''
+  return axios.get(`/api/Filters/Directors?search=${searchEncoded}`)
+}
