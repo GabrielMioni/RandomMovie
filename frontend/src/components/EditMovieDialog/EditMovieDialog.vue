51d5d30ee772c51bc656508a2ff4b49b8ebfb663
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-04-27 18:12:53 -0700

    Moved EditMovieDialog to components/EditMovieDialog

---
+++ frontend/src/components/EditMovieDialog/EditMovieDialog.vue
@@ -0,0 +1,93 @@
+<template>
+  <v-dialog
+    max-width="1200"
+    :fullscreen="$vuetify.breakpoint.smAndDown"
+    v-model="open">
+    <v-card>
+      <v-toolbar
+        color="primary"
+        flat>
+        <v-spacer></v-spacer>
+        <v-btn
+          color="white"
+          icon
+          @click="open = false">
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+      </v-toolbar>
+      <v-card-text>
+        <v-form>
+          <v-container>
+            <v-row>
+              <v-col>
+                <v-text-field
+                  v-model="movieLocal.title"
+                  label="Title"
+                  outlined>
+                </v-text-field>
+              </v-col>
+              <v-col>
+                <v-text-field
+                  v-model="movieLocal.year"
+                  label="Year"
+                  outlined>
+                </v-text-field>
+              </v-col>
+            </v-row>
+            <v-row dense>
+              <directors-combobox
+                :movie="movie">
+              </directors-combobox>
+            </v-row>
+          </v-container>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import DirectorsCombobox from '@/components/EditMovieDialog/DirectorsCombobox.vue'
+
+export default {
+  name: 'EditMovieDialog',
+  components: {
+    DirectorsCombobox
+  },
+  data () {
+    return {
+      movieLocal: {},
+      selectedDirectors: [],
+      directorSearchTimeout: null,
+      inputElm: null
+    }
+  },
+  props: {
+    value: {
+      required: true,
+      type: Boolean
+    },
+    movie: {
+      required: true,
+      type: Object
+    }
+  },
+  mounted () {
+    this.movieLocal = { ...this.movie }
+  },
+  computed: {
+    open: {
+      get () {
+        return this.value
+      },
+      set (value) {
+        this.$emit('input', value)
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
