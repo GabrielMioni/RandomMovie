5c1ab3e4b6fa3cf29bab16b25a3683cc569ad448
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-04-28 19:24:07 -0700

    Added GenresCombobox

---
+++ frontend/src/components/EditMovieDialog/GenresCombobox.vue
@@ -0,0 +1,57 @@
+<template>
+  <combobox-server-side
+    v-model="genreSearch"
+    :items="genres"
+    label="Genres"
+    :selected-items="selectedGenres"
+    @selectedItemsUpdate="updateSelectedItems">
+  </combobox-server-side>
+</template>
+
+<script>
+import ComboboxServerSide from '@/components/ComboboxServerSide.vue'
+import { mapActions, mapGetters } from 'vuex'
+
+export default {
+  name: 'GenresCombobox',
+  components: { ComboboxServerSide },
+  data () {
+    return {
+      genreSearch: '',
+      selectedGenres: [],
+      movieLocal: {}
+    }
+  },
+  props: {
+    value: {
+      type: Array,
+      required: true
+    },
+    movie: {
+      required: true,
+      type: Object
+    }
+  },
+  computed: {
+    ...mapGetters('admin', ['genres'])
+  },
+  mounted () {
+    this.movieLocal = { ...this.movie }
+    this.selectedGenres = [...this.movie.genres]
+    if (this.genres.length <= 0) {
+      this.setGenres()
+    }
+  },
+  methods: {
+    ...mapActions('admin', ['setGenres']),
+    updateSelectedItems (value) {
+      this.selectedGenres = value
+      this.$emit('input', value)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
