c8d22ce6c555cb46229e4a8c034a5372a6ac4835
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-05-08 16:39:30 -0700

    Added searchPeople method in /api/metas.js

--- frontend/src/api/meta.js
+++ frontend/src/api/meta.js
@@ -3,3 +3,8 @@ import axios from 'axios'
 export const getConfigurationDetails = () => {
   return axios.get('/api/MetaData/Get')
 }
+
+export const searchPeople = (search = '') => {
+  const searchEncoded = search.trim().length > 0 ? encodeURI(search.trim()) : ''
+  return axios.get(`/api/MetaData/People?search=${searchEncoded}`)
+}
