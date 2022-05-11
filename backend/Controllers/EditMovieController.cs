2ac3102c822b765627320a55cdb8640f68bbed01
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-05-10 20:37:40 -0700

    Added EditMovieController

---
+++ backend/Controllers/EditMovieController.cs
@@ -0,0 +1,17 @@
+﻿using backend.Requests;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EditMovieController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult blah (EditMoviePayload data)
+        {
+            return Ok(data);
+        }
+    }
+}
