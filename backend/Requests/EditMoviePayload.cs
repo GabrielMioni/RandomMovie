c2b21fa5c62698ed6df3b343377fb66036cd9134
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-05-10 20:37:27 -0700

    Added EditMoviePayload

---
+++ backend/Requests/EditMoviePayload.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace backend.Requests
+{
+    public class EditMoviePayload
+    {
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public List<int> DirectorIds { get; set; }
+        public List<int> GenreIds { get; set; }
+        public List<int> CreditIds { get; set; }
+    }
+}
