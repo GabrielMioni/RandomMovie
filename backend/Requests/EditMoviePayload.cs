e68376e59a3d8d25292bf0819cc0e46cf7d52eea
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-05-18 21:52:39 -0700

    Added CountryId, CreditIds, DirectorIds, GenreIds to EditMoviePayload

--- backend/Requests/EditMoviePayload.cs
+++ backend/Requests/EditMoviePayload.cs
@@ -4,10 +4,12 @@ namespace backend.Requests
 {
     public class EditMoviePayload
     {
-        public string Title { get; set; }
-        public int Year { get; set; }
+        public int CountryId { get; set; }
+        public List<int> CreditIds { get; set; }
         public List<int> DirectorIds { get; set; }
         public List<int> GenreIds { get; set; }
-        public List<int> CreditIds { get; set; }
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
     }
 }
