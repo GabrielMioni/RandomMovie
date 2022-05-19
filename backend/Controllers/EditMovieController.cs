be9dc70decfdf1aa869137e49fc46a4ae0645640
Gabriel Mioni <gabriel.mioni@gmail.com>
2022-05-18 21:51:26 -0700

    EditMovieController sets movieService in constructor

--- backend/Controllers/EditMovieController.cs
+++ backend/Controllers/EditMovieController.cs
@@ -1,4 +1,5 @@
 ﻿using backend.Requests;
+using backend.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,9 +9,18 @@ namespace backend.Controllers
     [ApiController]
     public class EditMovieController : ControllerBase
     {
-        [HttpPost]
-        public IActionResult blah (EditMoviePayload data)
+        private MovieService _movieService;
+
+        public EditMovieController(MovieService movieService)
         {
+            _movieService = movieService;
+        }
+
+        [HttpPost]
+        public IActionResult EditMovie (EditMoviePayload data)
+        {
+            _movieService.EditMovie(data);
+
             return Ok(data);
         }
     }
